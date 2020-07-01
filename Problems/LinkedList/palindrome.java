8b5cbad843a15f8534054c550f8ddede358c143d
Calvin Gao <c_gao3@yahoo.com>
2020-06-30 20:26:21 -0700

    palindrome

---
+++ Problems/LinkedList/palindrome.java
@@ -0,0 +1,51 @@
+/* Palindrome Linkedlist
+Given a singly linked list, determine if it is a palindrome.
+
+Example 1:
+
+Input: 1->2
+Output: false
+Example 2:
+
+Input: 1->2->2->1
+Output: true
+Follow up:
+Could you do it in O(n) time and O(1) space?*/
+package Problems.LinkedList;
+
+public class palindrome {
+    public boolean isPalindrome(ListNode head) 
+    {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        slow = reversed(slow);
+        fast = head;
+
+        while(slow != null){
+            if(slow.val != fast.val){
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+
+    public ListNode reversed(ListNode head)
+    {
+        ListNode prev = null;
+        while( head != null){
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}

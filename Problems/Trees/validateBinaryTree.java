e7772b56387a661a4b7ea03a709d5409c29324c1
Calvin Gao <c_gao3@yahoo.com>
2020-07-01 18:48:55 -0700

    validate tree

---
+++ Problems/Trees/validateBinaryTree.java
@@ -0,0 +1,16 @@
+package Problems.Trees;
+
+public class validateBinaryTree {
+    public boolean isValidBST(TreeNode root) {
+        return isValid(root, null, null);
+    }
+
+    public boolean isValid(TreeNode root, Integer min , Integer max)
+    {
+        if(root == null) return true;
+        if(min != null && root.val <= min) return false;
+        if(max != null && root.val >= max) return false;
+
+        return isValid(root.left,min,root.val) && isValid(root.right,root.val, max);
+    }
+}

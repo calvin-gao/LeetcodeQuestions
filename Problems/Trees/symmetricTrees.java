e742135d39d2f3f1ca198d506873f678219e1c80
Calvin Gao <c_gao3@yahoo.com>
2020-07-02 19:33:17 -0700

    symmetrictrees

---
+++ Problems/Trees/symmetricTrees.java
@@ -0,0 +1,36 @@
+/*Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+ 
+
+But the following [1,2,2,null,3,null,3] is not:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+ 
+
+Follow up: Solve it both recursively and iteratively.*/
+
+package Problems.Trees;
+
+public class symmetricTrees {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        return isMirror(root.left, root.right);
+    }
+    
+    public boolean isMirror(TreeNode p , TreeNode q){
+        if(p==null && q==null) return true;
+        if(p==null || q==null) return false;
+        return (p.val == q.val) && isMirror(p.left, q.right) && isMirror(p.right, q.left);
+    }
+}

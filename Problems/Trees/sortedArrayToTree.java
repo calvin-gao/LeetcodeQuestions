b312a917e516f33bbe9188cac5271ebd199feda0
Calvin Gao <c_gao3@yahoo.com>
2020-07-03 20:59:39 -0700

    test

--- Problems/Trees/sortedArrayToTree.java
+++ Problems/Trees/sortedArrayToTree.java
@@ -22,5 +22,16 @@ One possible answer is: [0,-3,9,-10,null,5], which represents the following heig
 
 public class sortedArrayToTree {
     public TreeNode sortedArrayToBST(int[] nums){
+        if(nums.length == 0) return null;
+        return constructTreeFromArray(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode constructTreeFromArray(int[] nums, int left, int right){
+        if(left > right) return null;
+        int midpoint = left + (right - left) / 2;
+        TreeNode node = new TreeNode(nums[midpoint]);
+        node.left = constructTreeFromArray(nums, left, midpoint - 1);
+        node.right = constructTreeFromArray(nums, midpoint+1, right);
+        return node;
     }
 }

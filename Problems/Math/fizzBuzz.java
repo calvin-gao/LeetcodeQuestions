92916a61780ec813ce812ce8778bdda1acae99a8
Calvin Gao <c_gao3@yahoo.com>
2020-07-09 20:00:11 -0700

    findPrimes

--- Problems/Math/fizzBuzz.java
+++ Problems/Math/fizzBuzz.java
@@ -4,6 +4,17 @@ import java.util.*;
 public class fizzBuzz {
     public List<String> fizzBuzz(int n) {
         List<String> output = new ArrayList<String>();
-
+        for(int i = 1; i<= n; i++){
+            if((i % 3 == 0) && (i % 5 == 0)){
+                output.add("FizzBuzz");
+            }else if( i % 3 == 0 ){
+                output.add("Fizz");
+            }else if( i % 5 == 0){
+                output.add("Buzz");
+            }else{
+                output.add(String.valueOf(i));
+            }
+        }
+        return output;
     }
 }

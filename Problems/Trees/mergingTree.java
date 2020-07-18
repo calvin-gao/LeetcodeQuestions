fcfe3c62c5783ce0a5a685c969681e5855cc9749
Calvin Gao <c_gao3@yahoo.com>
2020-07-17 19:58:50 -0700

    update

--- Problems/Trees/mergingTree.java
+++ Problems/Trees/mergingTree.java
@@ -5,7 +5,7 @@ public class mergingTree {
         if(t1 == null && t2 == null) return null;
         if(t1 == null) return t2;
         if(t2 == null) return t1;
-        TreeNode t = new TreeNode(t1.val + t2.val);
+        TreeNode t = new TreeNode(t1.val + t2.val); 
         t.left = mergeTrees(t1.left , t2.left);
         t.right = mergeTrees(t1.right, t2.right);
         return t;
